a9ce614931a6a9ae27295d869634da3ddd3f22bf
Nikita Bai <oceandev000@gmail.com>
2022-01-09 04:06:53 -0800

    update

--- screen/QRcodeScanner/TabScreen/DriverScreen/index.js
+++ screen/QRcodeScanner/TabScreen/DriverScreen/index.js
@@ -1,2 +1,9 @@
 import React from "react";
-
+import {View, Text} from 'react-native';
+const DriverScreen = () => {
+    return(
+        <View>
+            <Text>O K</Text>
+        </View>
+    )
+}
